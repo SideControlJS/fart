491817b4f6a14ac14db9ff70100ab8356cd37eab
Jesse <jesse.lare@gmail.com>
2024-02-08 00:07:10 -0500

    adding YouTube API logic and Twitch API logic

--- server.js
+++ server.js
@@ -12,12 +12,14 @@ app.use(cors());
 // Routes
 // YouTube API route
 app.get('/api/youtube-shorts', async (req, res) => {
-    // ... (YouTube API call logic here)
+    const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=10&playlistId=YOUR_PLAYLIST_ID&key=${process.env.YOUTUBE_API_KEY}`;
+
 });
 
 // Twitch API route
 app.get('/api/twitch-clips', async (req, res) => {
-    // ... (Twitch API call logic here)
+    const twitchApiUrl = `https://api.twitch.tv/helix/streams?user_login=YOUR_TWITCH_USERNAME`;
+
 });
 
 app.listen(port, () => {
