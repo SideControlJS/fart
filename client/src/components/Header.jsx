28e5d6691bc7626e39ac31c725c136fd296ccec9
Jesse <jesse.lare@gmail.com>
2024-02-10 13:48:36 -0500

    added Header Component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+// src/components/Header.jsx
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>Streamer's Hub</h1>
+      {/* You can add navigation or logo here */}
+    </header>
+  );
+}
+
+export default Header;
